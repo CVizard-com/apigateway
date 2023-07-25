2c069d5a5d0d70f45da4c8442b7db49b37da99f3
Paweł Solecki <pawelll.solecki@gmail.com>
2023-07-25 20:35:12 +0200

    api gateway ssl

--- src/main/java/com/cvizard/apigateway/ApigatewayApplication.java
+++ src/main/java/com/cvizard/apigateway/ApigatewayApplication.java
@@ -35,7 +35,7 @@ public class ApigatewayApplication {
     private Connector httpToHttpsRedirectConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setPort(8082);
+        connector.setPort(8080);
         connector.setSecure(false);
         connector.setRedirectPort(8443);
         return connector;
