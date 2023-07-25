6219e8caf068684b97a50371d472546bbfe7a246
Paweł Solecki <pawelll.solecki@gmail.com>
2023-07-25 20:14:52 +0200

    api gateway ssl

--- src/main/java/com/cvizard/apigateway/ApigatewayApplication.java
+++ src/main/java/com/cvizard/apigateway/ApigatewayApplication.java
@@ -1,7 +1,13 @@
 package com.cvizard.apigateway;
 
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.apache.catalina.Context;
 
 @SpringBootApplication
 public class ApigatewayApplication {
@@ -9,4 +15,21 @@ public class ApigatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayApplication.class, args);
     }
-}
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        return tomcat;
+        }
+
+    }
+
